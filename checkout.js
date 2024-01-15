a1b57d786d006d7ae2da6a29dbb95ac455c1cf03
Ellena <lapunovael05@gmail.com>
2024-01-15 19:11:02 +0000

    2

---
+++ checkout.js
@@ -0,0 +1,36 @@
+const elements = {
+  list: document.querySelector(".js-list"),
+  cost: document.querySelector(".js-cost"),
+  clear: document.querySelector(".js-clear"),
+};
+
+
+const LS_KEY = "checkout";
+const products = JSON.parse(localStorage.getItem(LS_KEY)) ?? [];
+let totalCost;
+
+if(products.length){
+    elements.clear.hidden = false;
+totalCost = products.reduce((acc,{qty, price})=> acc + qty* price,0);
+}
+
+elements.cost.textContent= totalCost?`Total cost ${totalCost}` 
+: "Your basket is empty";
+function createMarkup(arr) {
+return arr.map(({img,name,qty,price})=> `
+<li class = "cart-item">
+<img src="${img}" alt="${name}" width="300">
+<h2>${name}</h2>
+<p>Quantity: ${qty}</p>
+<p>Total price: ${qty * price}</p>
+</li>`
+)
+.join('')
+
+} 
+elements.list.insertAdjacentHTML('afterbegin',createMarkup(products));
+elements.clear.addEventListener('click', handlerClear);
+function handlerClear() {
+localStorage.removeItem(LS_KEY)
+location.href = './index.html';
+}
